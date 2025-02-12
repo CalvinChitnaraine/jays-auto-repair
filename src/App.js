b4cfe039076b326af8e93be8d2e9603df4296d90
CalvinChitnaraine <cchitnaraine@gmail.com>
2025-02-12 14:25:47 -0500

    Created basic pages for navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Services from "./pages/services";
+import Contact from "./pages/contact";
+import Booking from "./pages/booking";
 
 function App() {
     return (
@@ -9,12 +12,18 @@ function App() {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/booking">Schedule Appointment</Link></li>
                 </ul>
             </nav>
 
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/booking" element={<Booking />} />
             </Routes>
         </Router>
     );
